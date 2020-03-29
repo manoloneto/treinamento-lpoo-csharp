cc3754f1a3701f4016911a724e46ac4737a6215f
Manoel Afonso <mano.afonso93@gmail.com>
2020-03-29 11:40:03 -0300

    Adicionando aula 03

---
+++ aula01/aula03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int media;
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Informe um número: ");
+                numero += Convert.ToInt32(Console.ReadLine());
+            }
+
+            media = numero / 10;
+
+            Console.WriteLine($"A média dos números é {media}!");
+        }
+    }
+}
