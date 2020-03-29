27ec9bc8021717bb427237a9f39af89730d71c5e
Manoel Afonso <mano.afonso93@gmail.com>
2020-03-29 14:08:53 -0300

    Adicionando aula 04

---
+++ aula01/aula04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+
+            while(idade < 18)
+            {
+                Console.WriteLine("Informe a sua idade: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
