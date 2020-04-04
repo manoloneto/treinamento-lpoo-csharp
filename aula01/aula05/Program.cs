f830179387afefcced650a4c517177005c8396d9
Manoel Afonso <mano.afonso93@gmail.com>
2020-04-04 16:48:00 -0300

    Adicionando aula 05

---
+++ aula01/aula05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace aula05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+
+            Console.WriteLine("Escolha uma opção:\n 1 ou 4 - Bom dia;\n 2 ou 3 - Boa tarde;");
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                case 4:
+                    Console.WriteLine("Bom dia usuário!");
+                    break;
+
+                case 2:
+                case 3:
+                    Console.WriteLine("Boa tarde usuário!");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+}
